ba09fabe224af100a71c8da4a08ae25d0b0dcf09
ajohnson1031 <aaron.johnson1031@gmail.com>
2020-05-16 12:16:26 -0400

    completed oop2 classes. tests run "OK"

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -3,10 +3,12 @@
 # Also change it so the num_wheels defaults to 4 if not specified when the
 # object is constructed.
 
-class GroundVehicle():
-    def __init__(self, num_wheels):
+class GroundVehicle:
+    def __init__(self, num_wheels=4):
         self.num_wheels = num_wheels
 
+    def drive(self):
+        return "vroooom"
     # TODO
 
 
@@ -19,6 +21,13 @@ class GroundVehicle():
 
 # TODO
 
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        self.num_wheels = num_wheels
+    
+    def drive(self):
+        return "BRAAAP!!"
+    
 vehicles = [
     GroundVehicle(),
     GroundVehicle(),
